cd83c64c0a5c291dccddf4959429c874a649c88d
Ritam Koley <146624672+RiKo-19@users.noreply.github.com>
2025-06-21 00:18:42 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -38,8 +38,3 @@ def chat(request: Requeststate):
     response = get_response_from_ai_agent(llm_id, query, allow_search, system_prompt, provider)
     return response
     
-# Run app & Explore Swagger UI Docs
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=9999)
